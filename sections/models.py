ad91b6bd0710ed0886b7ff643db791b1b211ce3c
Ilya Seleznev <zedplay.zp@gmail.com>
2024-12-02 01:49:46 +0300

    Added models for sections app

--- sections/models.py
+++ sections/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from users.models import NULLABLE
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Section(models.Model):
+    title = models.CharField(max_length=150, verbose_name=_('title'))
+    description = models.TextField(verbose_name=_('description'), **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('section')
+        verbose_name_plural = _('sections')
+        ordering = ['id']
+
+class SectionContent(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name=_('section'))
+    title = models.CharField(max_length=150, verbose_name=_('title'))
+    content = models.TextField(verbose_name=_('content'))
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('section content')
+        verbose_name_plural = _('section contents')
+        ordering = ['id']
