77f869ca54473837de165533fc58783c5b999f4d
Ilya Seleznev <zedplay.zp@gmail.com>
2024-12-02 01:49:23 +0300

    Added paginators for sections app

---
+++ sections/paginators.py
@@ -0,0 +1,11 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class SectionPaginator(PageNumberPagination):
+    page_size = 3
+    page_query_param = 'page_size'
+    max_page_size = 10
+
+
+class SectionContentPaginator(PageNumberPagination):
+    page_size = 10
