d416166d4fb6890ffdd01520dc748642f093f31d
Ilya Seleznev <zedplay.zp@gmail.com>
2024-12-02 20:22:00 +0300

    Added test model with migration for sections

--- sections/models.py
+++ sections/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from users.models import NULLABLE
 from django.utils.translation import gettext_lazy as _
 
+
 class Section(models.Model):
     title = models.CharField(max_length=150, verbose_name=_('title'))
     description = models.TextField(verbose_name=_('description'), **NULLABLE)
@@ -13,6 +14,7 @@ class Section(models.Model):
         verbose_name = _('section')
         verbose_name_plural = _('sections')
         ordering = ['id']
+
 
 class SectionContent(models.Model):
     section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name=_('section'))
@@ -26,3 +28,18 @@ class SectionContent(models.Model):
         verbose_name = _('section content')
         verbose_name_plural = _('section contents')
         ordering = ['id']
+
+
+class Tests(models.Model):
+    test_section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name=_('test section'))
+    description = models.TextField(verbose_name=_('description'), **NULLABLE)
+    question = models.TextField(verbose_name=_('question'), **NULLABLE)
+    answer = models.TextField(max_length=40, verbose_name=_('answer'), **NULLABLE)
+
+    def __str__(self):
+        return f'Тест по курсу {self.test_section.title}'
+
+    class Meta:
+        verbose_name = _('test')
+        verbose_name_plural = _('tests')
+        ordering = ['test_section']
