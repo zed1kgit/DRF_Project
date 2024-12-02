796d80778e3b1491edb9380a8a464d2376e894e8
Ilya Seleznev <zedplay.zp@gmail.com>
2024-12-02 20:22:20 +0300

    Added admin functional for sections

--- sections/admin.py
+++ sections/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from sections.models import Section, SectionContent, Tests
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    list_filter = ('id',)
+    ordering = ('id',)
+
+
+@admin.register(SectionContent)
+class SectionContentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'section',)
+    list_filter = ('id', 'section',)
+    ordering = ('id', 'section',)
+
+
+@admin.register(Tests)
+class TestsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'test_section', 'description', 'question', 'answer',)
+    list_filter = ('id', 'test_section',)
+    ordering = ('id', 'test_section',)
