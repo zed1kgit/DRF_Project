9cc68c9bbf8992f1ff7ce6543a2a4e3897fc8b6a
Ilya Seleznev <zedplay.zp@gmail.com>
2024-12-05 00:08:10 +0300

    Added user retrieve

--- users/urls.py
+++ users/urls.py
@@ -3,13 +3,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.views.decorators.cache import never_cache
 
 from users.apps import UsersConfig
-from users.views import UserListAPIView, UserCreateAPIView, UserDestroyAPIView, UserUpdateAPIView
+from users.views import UserListAPIView, UserCreateAPIView, UserDestroyAPIView, UserUpdateAPIView, UserRetrieveAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     # user urlpatterns
     path('', UserListAPIView.as_view(), name='user-list'),
+    path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user-detail'),
     path('create/', never_cache(UserCreateAPIView.as_view()), name='user-create'),
     path('<int:pk>/delete/', UserDestroyAPIView.as_view(), name='user-delete'),
     path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user-update'),
